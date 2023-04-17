65888fc60b51d0b200ed5c8c921300135c8a2bb2
Ajay481 <iamajayyadav481@gmail.com>
2023-04-17 19:33:27 +0530

    episode-3

--- App.js
+++ App.js
@@ -1,39 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// Render Page using React
-/**
- * <div id="parent">
- *    <div id="child">
- *      <h1>I am h1 tag</h1>
- *      <h2>I am h2 tag</h2>
- *    </div>
- *    <div id="child2">
- *      <h1>I am h1 tag</h1>
- *      <h2>I am h2 tag</h2>
- *    </div>
- * </div>
- */
+// React.createElement => ReactElement- JS Object => HTMLElement(render)
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", xyz: "abc" }, // Attributes
-//   "Hello World By React!"
-// );
+//This is how can create React element using React
+// const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
 
-const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-]);
+// JSX => HTML-like or XML-like syntax but not HTML or XML in Javascript
 
-console.log(heading); // Object
+//This is how can create React element using JSX
+//JSX (transpiled before it reaches the JS engine) - PARCEL - Babel
+//JSX => Babel transpile it to React.createElement => ReactElement- JS Object => HTMLElement(render)
+const heading = <h1 id="heading">Namaste React using JSX</h1>;
+const Heading = () => <h2 id="heading">Namaste React using JSX</h2>;
 
+// React Component
+// 1. Class Based Component - OLD
+// 2. Functional Component - NEW
+
+const HeadingComponent = () => {
+  return (
+    <div>
+      {heading}
+      <Heading />
+      <Heading></Heading>
+      {Heading()}
+      <h1>Namaste React Functional Component</h1>
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const root2 = ReactDOM.createRoot(document.getElementById("root2"));
 
-root.render(heading); // Take this Object create h1 tag which browser understands and put that inside the root
+root.render(<HeadingComponent />);
+root2.render(<Heading />);
